3711f17bc879526b792aa60bab5d069a9df9bc3b
Gustavo Mello Soares dos Santos <gustavo@digicom.com.br>
2025-07-21 19:12:20 -0300

    Criando o Menu ou "nav"

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Menu from "./componentes/menu";
 import Inicio from "./paginas/inicio";
 import SobreMim from "./paginas/sobreMim";
 
@@ -6,6 +7,8 @@ import SobreMim from "./paginas/sobreMim";
 function App() {
   return(
     <BrowserRouter>
+      <Menu />
+
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/sobremim" element={<SobreMim />} />
