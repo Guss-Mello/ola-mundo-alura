5c7cf3988d419acc2afde61750c444aae5d12417
Gustavo Mello Soares dos Santos <gustavo@digicom.com.br>
2025-07-30 19:20:41 -0300

    Criado o componente PostModelo

--- src/paginas/sobreMim/index.js
+++ src/paginas/sobreMim/index.js
@@ -1,5 +1,55 @@
+import styles from './SobreMim.module.css';
+import PostModelo from 'componentes/PostModelo';
+import fotoCapa from 'assets/sobre_mim_capa.png';
+import fotoSobreMim from 'assets/sobre_mim_foto.png';
+
 export default function SobreMim() {
     return(
-        <h1>Sobre Mim</h1>
+        <PostModelo fotoCapa={fotoCapa} titulo="Sobre Mim">
+            <h3 className={styles.subtitulo}>
+                Ola, eu sou o Guss!
+            </h3>
+
+            <img src={fotoSobreMim} alt='Foto do mano ali sorrindo' className={styles.fotoSobreMim}/>
+
+            <p className={styles.paragrafo}>
+                Paragrafo teste ai dps eu escrevo algo aqui e tudo mais
+            </p>
+            <p className={styles.paragrafo}>
+                Paragrafo teste ai dps eu escrevo algo aqui e tudo mais    
+            </p>
+            <p className={styles.paragrafo}>
+                Paragrafo teste ai dps eu escrevo algo aqui e tudo mais    
+            </p>
+            <p className={styles.paragrafo}>
+                Paragrafo teste ai dps eu escrevo algo aqui e tudo mais    
+            </p>
+            <p className={styles.paragrafo}>
+                Paragrafo teste ai dps eu escrevo algo aqui e tudo mais    
+            </p>
+            <p className={styles.paragrafo}>
+                Paragrafo teste ai dps eu escrevo algo aqui e tudo mais    
+            </p>
+            <p className={styles.paragrafo}>
+                Paragrafo teste ai dps eu escrevo algo aqui e tudo mais   
+            </p>
+            <p className={styles.paragrafo}>
+                Paragrafo teste ai dps eu escrevo algo aqui e tudo maisParagrafo teste ai dps eu escrevo algo aqui e tudo maisParagrafo teste ai dps eu escrevo algo aqui e tudo maisParagrafo teste ai dps eu escrevo algo aqui e tudo maisParagrafo teste ai dps eu escrevo algo aqui e tudo maisParagrafo teste ai dps eu escrevo algo aqui e tudo mais
+            </p>
+            <p className={styles.paragrafo}>
+                Paragrafo teste ai dps eu escrevo algo aqui e tudo mais    
+            </p>
+            <p className={styles.paragrafo}>
+                Paragrafo teste ai dps eu escrevo algo aqui e tudo mais    
+            </p>
+            <p className={styles.paragrafo}>
+                Paragrafo teste ai dps eu escrevo algo aqui e tudo maisParagrafo teste ai dps eu escrevo algo aqui e tudo maisParagrafo teste ai dps eu escrevo algo aqui e tudo maisParagrafo teste ai dps eu escrevo algo aqui e tudo mais
+            </p>
+            <p className={styles.paragrafo}>
+                Paragrafo teste ai dps eu escrevo algo aqui e tudo mais Paragrafo teste ai dps eu escrevo algo aqui e tudo maisParagrafo teste ai dps eu escrevo algo aqui e tudo maisParagrafo teste ai dps eu escrevo algo aqui e tudo maisParagrafo teste ai dps eu escrevo algo aqui e tudo maisParagrafo teste ai dps eu escrevo algo aqui e tudo mais   
+            </p>
+
+
+        </PostModelo>
     )
 }
